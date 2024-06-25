da224a866bd7d90b8a578ac9397e816700026d63
divmkw <87936208+divmkw@users.noreply.github.com>
2024-06-25 15:31:50 +0530

    Add files via upload

---
+++ frontend/src/components/index.js
@@ -0,0 +1,22 @@
+// Navbar
+export { default as Navbar } from './navbar/Navbar'
+// Footer
+export { default as Footer } from './footer/Footer'
+// Home Page
+export { default as Home } from './home/Home'
+// Movies Page
+export { default as Movies } from './movies/Movies'
+// User Login Page
+export { default as Auth } from './auth/Auth'
+// Admin Page
+export { default as Admin } from './auth/Admin'
+// Booking Page
+export { default as Booking } from './booking/Booking'
+// Add Movies Page
+export { default as AddMovie } from './addmovie/AddMovie'
+// Not found
+export { default as NotFound } from './notfound/NotFound'
+// Popup pages
+export { default as AddMoviePopup } from './popup/AddMoviePopup'
+export { default as TicketBookedPopup } from './popup/TicketBookedPopup'
+export { default as CancelTicket } from './popup/CancelTicket'
