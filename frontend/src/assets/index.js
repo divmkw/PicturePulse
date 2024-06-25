35bc435d1155d90f7c902b94be8f308e9880ef86
divmkw <87936208+divmkw@users.noreply.github.com>
2024-06-25 15:30:16 +0530

    Add files via upload

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,12 @@
+const assets = {
+   
+    ProfileImg: require('./images/profile-img.jpg'),
+    MainLogo: require('./logos/picturepulse-logo-transparent.png'),
+    WallBackGround: require('./backgrounds/black-grunge-brick-wall-background.jpg'),
+    Glasses: require('./images/glasses.png'),
+    Popcorn1: require('./images/popcorn1.png'),
+    Popcorn2: require('./images/popcorn2.png'),
+    theatre_bg: require('./backgrounds/theatre_bg.jpg')
+}
+
+export default assets;
